81d3b1cea328ffeb2ee0fc0d9b056736272e73dc
Shadab Khan <shadab.2005@gmail.com>
2020-11-08 19:48:51 +0530

    Add tailwindcss support

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { AppProps } from "next/app";
-import "../styles/globals.css";
+import "../styles/globals.scss";
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
